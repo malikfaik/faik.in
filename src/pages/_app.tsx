a40288f6695d65591eabd114c9a0776a8157372b
Faik Malik <faik.malik@trivago.com>
2021-01-02 11:57:44 +0100

    feat(src/pages, src/stylesheets): added the main page and fixed the import for _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import '../stylesheets/footer-app.scss'
 import '../stylesheets/header-app.scss'
 import '../stylesheets/cover-picture.scss'
 import '../stylesheets/section.scss'
-import '../stylesheets/experience.scss'
 import '../stylesheets/index.scss'
 
 function MyApp({ Component }: { Component: new () => React.Component }): React.ReactElement {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,65 +1,41 @@
-import Head from 'next/head'
-import styles from '../stylesheets/Home.module.css'
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import Footer from '../components/footer-app'
+import Header from '../components/header-app'
+import Cover from '../components/cover-picture'
+import About from '../components/about-section'
+import Experience from '../components/experience-section'
+import Tech from '../components/tech-section/main'
+import Contact from '../components/contact-section'
 
-export default function Home() {
+const Home = (): React.ReactElement => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div className="home-container">
+      <Grid container>
+        <Grid className="header-fixed" container item xs={12} sm={12} md={12} lg={12} xl={12}>
+          <Header />
+        </Grid>
+        <Grid container item xs={12} sm={12} md={12} lg={12} xl={12}>
+          <Cover />
+        </Grid>
+        <Grid container item xs={12} sm={12} md={12} lg={12} xl={12}>
+          <About />
+        </Grid>
+        <Grid container item xs={12} sm={12} md={12} lg={12} xl={12}>
+          <Experience />
+        </Grid>
+        <Grid container item xs={12} sm={12} md={12} lg={12} xl={12}>
+          <Tech />
+        </Grid>
+        <Grid container item xs={12} sm={12} md={12} lg={12} xl={12}>
+          <Contact />
+        </Grid>
+        <Grid container item xs={12} sm={12} md={12} lg={12} xl={12}>
+          <Footer />
+        </Grid>
+      </Grid>
     </div>
   )
 }
+
+export default Home
