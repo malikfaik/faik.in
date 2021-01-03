5cf3aab18925f897aed0f60659c67c461ba0e2d6
Faik Malik <faik.malik@trivago.com>
2021-01-03 11:33:40 +0100

    feat(src/components/about-section): refactored the component and extracted the styles into jss

---
+++ src/components/about-section/about-content.ts
@@ -0,0 +1,5 @@
+export default {
+  body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Natoque penatibus et magnis dis parturient. Cras semper auctor
+    neque vitae tempus quam pellentesque nec. Tincidunt arcu non sodales neque sodales ut etiam sit amet. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Risus commodo viverra
+    maecenas accumsan lacus vel facilisis volutpat. Scelerisque viverra mauris in aliquam sem fringilla.`
+}
---
+++ src/components/about-section/main.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import useAboutSectionStyle from './styles'
+import sectionColumnSize from '../../config/section'
+import useSectionStyle from '../common/styles/section-style'
+import aboutContent from './about-content'
+
+const About = (): React.ReactElement => {
+  const sectionClasses = useSectionStyle()
+  const aboutClasses = useAboutSectionStyle()
+  return (
+    <>
+      <Grid className={sectionClasses.sectionHead} item {...sectionColumnSize}>
+        <span>About</span>
+      </Grid>
+
+      <Grid className={`${aboutClasses.aboutBody} ${sectionClasses.sectionBody}`} item {...sectionColumnSize}>
+        <p>{aboutContent.body}</p>
+      </Grid>
+    </>
+  )
+}
+
+export default About
