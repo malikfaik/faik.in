fb7853888e6317c5c3700055422f9dfb503c5a2a
Faik Malik <faik.malik@trivago.com>
2021-01-09 12:57:26 +0100

    chore(src/pages): added cssBaseline and enabled gloabl classNames

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { AppProps } from 'next/dist/next-server/lib/router/router'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import './app.css'
+
+const generateClassName = createGenerateClassName({
+  disableGlobal: false,
+  productionPrefix: 'c'
+})
 
 function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
   return (
     <>
-      <CssBaseline />
-      <Component {...pageProps} />
+      <StylesProvider generateClassName={generateClassName}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </StylesProvider>
     </>
   )
 }
