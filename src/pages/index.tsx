040750f0bc733059dda655f22da2f9faccc0f009
Faik Malik <faik.malik@trivago.com>
2021-01-13 18:49:16 +0100

    feat(src/pages): added refs and usescroll to main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { MutableRefObject, useRef } from 'react'
 import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles'
 import Header from '../components/header-section/main'
@@ -7,34 +7,45 @@ import About from '../components/about-section/main'
 import Experience from '../components/experience-section/main'
 import Tech from '../components/tech-section/main'
 import Contact from '../components/contact-section/main'
+import { useScrollAnimation } from '../components/common-components/on-scroll-spring/main'
 
 const usePageStyle = makeStyles({
   headerFixed: {
-    position: 'fixed'
+    position: 'fixed',
+    zIndex: 2
   }
 })
 
 const Home = (): React.ReactElement => {
+  const refs: { [name: string]: MutableRefObject<HTMLDivElement> } = {
+    Welcome: useRef(null),
+    About: useRef(null),
+    Experience: useRef(null),
+    Tech: useRef(null),
+    Contact: useRef(null)
+  }
+
   const pageStyle = usePageStyle()
+  useScrollAnimation({ initialValue: 0, refs })
 
   return (
     <Grid container>
       <Grid className={pageStyle.headerFixed} container>
-        <Header />
+        <Header {...refs} />
       </Grid>
-      <Grid container>
+      <Grid ref={refs.Welcome} container>
         <Welcome />
       </Grid>
-      <Grid container>
+      <Grid ref={refs.About} container>
         <About />
       </Grid>
-      <Grid container>
+      <Grid ref={refs.Experience} container>
         <Experience />
       </Grid>
-      <Grid container>
+      <Grid ref={refs.Tech} container>
         <Tech />
       </Grid>
-      <Grid container>
+      <Grid ref={refs.Contact} container>
         <Contact />
       </Grid>
     </Grid>
