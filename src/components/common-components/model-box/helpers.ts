cdec81be3039f07e92ff83847f86a09f856112f1
Faik Malik <faik.malik@trivago.com>
2021-01-14 22:45:33 +0100

    feat(src/components/common-components): added model-box component

---
+++ src/components/common-components/model-box/helpers.ts
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import { useSpring, SpringValue } from 'react-spring'
+
+type ModelData = {
+  img: string
+  description: string
+  name: string
+  duration: string
+}
+
+export type AnimationHook = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  animatedStyle: { scale: SpringValue<number>; opacity: SpringValue<any> }
+  modelData: ModelData
+}
+
+const animationHook = {
+  setStyleSpring: null,
+  setModelData: null
+}
+
+export const onOpen = (modelData: ModelData): void => {
+  animationHook.setModelData(modelData)
+  animationHook.setStyleSpring({ scale: 1, opacity: 1 })
+}
+
+export const onClose = (): void => {
+  animationHook.setStyleSpring({ scale: 0, opacity: 0 })
+}
+
+export const useAnimationHook = (): AnimationHook => {
+  const [animatedStyle, setStyleSpring] = useSpring(() => ({ scale: 0, opacity: 0 }))
+  const [modelData, setModelData] = useState({ img: null, description: null, name: null, duration: null })
+  animationHook.setStyleSpring = setStyleSpring
+  animationHook.setModelData = setModelData
+  return {
+    animatedStyle,
+    modelData
+  }
+}
---
+++ src/components/common-components/model-box/styles.ts
@@ -0,0 +1,130 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+const useModelBoxStyles = makeStyles(theme => ({
+  modelBoxWrapper: {
+    backgroundColor: 'rgba(250,250,250,0.7)',
+    bottom: 0,
+    height: '100vh',
+    left: 0,
+    margin: 'auto',
+    opacity: 0,
+    position: 'fixed',
+    right: 0,
+    top: 0,
+    transform: 'scale(0)',
+    width: '100vw',
+    zIndex: 3
+  },
+
+  modelBoxBody: {
+    height: '70vh',
+
+    [theme.breakpoints.between('xs', 'md')]: {
+      height: '50vh'
+    }
+  },
+
+  modelBoxHead: {
+    height: '70vh',
+
+    [theme.breakpoints.between('xs', 'md')]: {
+      height: '20vh'
+    }
+  },
+
+  modelBoxContent: {
+    alignItems: 'center',
+    display: 'flex',
+    flexWrap: 'wrap',
+    fontFamily: `'Quicksand', sans-serif`,
+    fontSize: '4vmin',
+    fontWeight: 400,
+    justifyContent: 'center',
+    overflow: 'hidden',
+    paddingLeft: '2vmin',
+    paddingTop: '2vmin',
+    textAlign: 'center'
+  },
+
+  modelBox: {
+    backgroundColor: '#fff',
+    bottom: 0,
+    height: '70vh',
+    left: 0,
+    margin: 'auto',
+    position: 'absolute',
+    right: 0,
+    top: 0,
+    width: '50vw',
+
+    [theme.breakpoints.between('xs', 'md')]: {
+      height: '70vh',
+      width: '80vw'
+    }
+  },
+
+  modelBoxClose: {
+    cursor: 'pointer',
+    fontSize: '5vmin',
+    marginLeft: '73%',
+    marginTop: '4.5%',
+
+    [theme.breakpoints.between('xs', 'md')]: {
+      fontSize: '8vmin',
+      marginLeft: '82%',
+      marginTop: '18%'
+    }
+  },
+
+  modelBoxLogo: {
+    objectFit: 'contain',
+    width: '40%',
+    [theme.breakpoints.between('xs', 'md')]: {
+      left: '10%',
+      position: 'absolute',
+      top: '9%',
+      width: '20%'
+    }
+  },
+
+  modelBoxName: {
+    width: '100%',
+    [theme.breakpoints.between('xs', 'md')]: {
+      fontSize: '6vmin',
+      marginBlockEnd: 0,
+      marginBlockStart: '1%',
+      marginLeft: '20%',
+      marginTop: '25%',
+      textAlign: 'justify'
+    }
+  },
+
+  modelBoxDuration: {
+    fontSize: '2vmin',
+    fontWeight: 700,
+    [theme.breakpoints.between('xs', 'md')]: {
+      fontSize: '4vmin',
+      marginBlockEnd: 0,
+      marginBlockStart: '1%',
+      marginLeft: '20%',
+      textAlign: 'justify',
+      width: '100%'
+    }
+  },
+
+  modelBoxDescription: {
+    fontSize: '2vmin',
+    overflow: 'hidden',
+    paddingLeft: '6vmin',
+    paddingRight: '6vmin',
+
+    textAlign: 'justify',
+
+    [theme.breakpoints.between('xs', 'md')]: {
+      fontSize: '4vmin',
+      height: '80%'
+    }
+  }
+}))
+
+export default useModelBoxStyles
