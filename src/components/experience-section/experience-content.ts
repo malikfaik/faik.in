deb6ab575f829cd2cd56ff692a0e5d791a0fe20f
Faik Malik <faik.malik@trivago.com>
2021-01-15 14:54:37 +0100

    fix(src/components): fixed safari issues with date

--- src/components/experience-section/experience-content.ts
+++ src/components/experience-section/experience-content.ts
@@ -7,7 +7,7 @@ export default [
     className: 'bullet',
     description:
       "I have been working at Trivago as a cross functional engineer, primarily handling Back-end and Dev-Ops topics.\nMy main work involves shipping new features and supporting the team with maintaince or sometimes upgrade ( you know it's a rewrite xD ) of legacy features or services in an agile environment, foster knowledge sharing within the team on different topics and collaborating with different teams while developing new features.",
-    from: new Date('01-15-2020'),
+    from: new Date('01-15-2020'.replace(/-/g, '/')),
     to: null
   },
   {
@@ -18,8 +18,8 @@ export default [
     className: 'bullet',
     description:
       "I Worked at Infino as a Lead, my work primarily involved building the entire Back-end from scratch ( Well I didn't xD, I had an amazing team).\nI also mentored / supported fellow engineers on different Back-end Topics, collaborated ( *crying* ) with different tech providers for integration.\nBeing a payments related start-up, the work had to be scrutinized thoroughly to be complaint with PCI and other regulations ( sed lyf )",
-    from: new Date('01-01-2019'),
-    to: new Date('12-01-2019')
+    from: new Date('01-01-2019'.replace(/-/g, '/')),
+    to: new Date('12-01-2019'.replace(/-/g, '/'))
   },
   {
     name: 'Accenture',
@@ -29,8 +29,8 @@ export default [
     className: 'bullet',
     description:
       'I Worked at Accenture as a Contractor, primarily handling data engineering topics.\nThe main work involved building new ETL pipelines and integration for Analytics, migrating older pipelines to a more robust and cleaner and testable alternative, building solutions for web scraping and designing performant schemas for databases.',
-    from: new Date('05-15-2018'),
-    to: new Date('09-15-2018')
+    from: new Date('05-15-2018'.replace(/-/g, '/')),
+    to: new Date('09-15-2018'.replace(/-/g, '/'))
   },
   {
     name: 'Freelance',
@@ -40,7 +40,7 @@ export default [
     className: 'bullet',
     description:
       "I have been freelancing for almost 7 years now.\nIn the freelancing space, my primary work involves... ( I am kidding ), it's freelancing, you do whatever pays xD, but still, I have worked on developing websites, apps, scripts, some infrastructure things here and there, pretty much everything tech related that comes my way, even graphic designing at one point xD ( I do have a sense of design, atleast good enough for a Software Engineer ).",
-    from: new Date('01-01-2013'),
+    from: new Date('01-01-2013'.replace(/-/g, '/')),
     to: null
   }
 ]
