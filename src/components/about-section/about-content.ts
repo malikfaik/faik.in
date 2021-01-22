ec3ddc9cbaea52c9db372eb64791b78ae8fefa4e
Faik Malik <faik.malik@trivago.com>
2021-01-22 11:02:49 +0100

    chore: updated content

--- src/components/about-section/about-content.ts
+++ src/components/about-section/about-content.ts
@@ -1,4 +1,4 @@
 export default {
   body:
-    "I am from Kashmir, India based in Munich, Germany. I started freelancing during later half of my school. I believe in being language agnostic, I treat language as a tool and not as a skill, the skill for me is investigating and designing solutions and using the best language available to implement that solution. \nI also put high importance on having fun while you work, I am a human not a robot, I enjoy my work and I make sure everyone else who works with me enjoy their's too."
+    'I am an experienced engineer having worked in a diverse set of industries and fields for more than 6 years. Currently, I am working as a Backend Engineer with Trivago N.V., I hail from Srinagar, Kashmir, which is the nothernmost part of India. On a random day you can find me either gaming, travelling or just fixing up some scripts. \nFrom engineering side of things, I believe in being language agnostic as that gives me the freedom to choose the best tool / language available for the job and not marrying a single technology for life.'
 }
