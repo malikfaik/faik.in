c32c98f86836def7c2557f84e5aea8d048ff1ea0
Faik Malik <faikmalik@pop-os.localdomain>
2021-03-27 15:32:11 +0100

    chore: updated about section

--- src/components/about-section/about-content.ts
+++ src/components/about-section/about-content.ts
@@ -1,4 +1,4 @@
 export default {
   body:
-    'I am an experienced engineer having worked in a diverse set of industries and fields for more than 6 years. Currently, I am working as a Backend Engineer with Trivago N.V . On a random day you can find me either gaming, travelling or just fixing up some scripts. \nFrom engineering side of things, I believe in being language agnostic as that gives me the freedom to choose the best tool / language available for the job and not marrying a single technology for life.'
+    'I am an experienced engineer having worked in a diverse set of industries and fields for more than 6 years. Currently, I am working as a Backend Engineer with Joyn Gmbh. On a random day you can find me either gaming, travelling or just fixing up some scripts. \nFrom engineering side of things, I believe in being language agnostic as that gives me the freedom to choose the best tool / language available for the job and not marrying a single technology for life.'
 }
