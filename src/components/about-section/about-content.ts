517aedf96feb73d895f63ee56a01cf5138210c2a
Faik Malik <malikfaik@gmail.com>
2021-08-12 07:20:35 +0200

    chore: improved copy for about section

--- src/components/about-section/about-content.ts
+++ src/components/about-section/about-content.ts
@@ -1,4 +1,4 @@
 export default {
   body:
-    'I am an experienced engineer who has worked in several different spaces over the last 6 years, I strongly believe in being a test first and a language agnostic engineer. As of now I am working with Joyn Gmbh in Munich, helping them make content pipelines more robust, performant and generic for our partners. \nWhen I am not upgrading legacy code or playing around with my pet projects, I am either playing games, travelling or just tinkering with some 3d printers and electronics.'
+    'I am an experienced engineer who has worked in several different spaces over the last 6 years, I strongly believe in being langauage agnostic and test driven when it comes to building or upgrading services. As of now I am working with Joyn Gmbh in Munich, helping them make content pipelines more robust, performant and generic for our partners. \nWhen I am not upgrading legacy code or playing around with my pet projects, I am either playing games, travelling or just tinkering with some 3d printers and electronics.'
 }
