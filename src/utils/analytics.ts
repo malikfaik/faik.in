4a8e283f18481711476658e2b5040c318bbb3653
Faik Malik <faik.malik@joyn.de>
2023-11-01 23:02:37 +0100

    fix: add more analytics

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -2,7 +2,7 @@ export const GA_TRACKING_ID = process.env.GAID
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string, dataObject: Record<string, string>): void => {
-  window.gtag('event', GA_TRACKING_ID, {
+  window.gtag('set', GA_TRACKING_ID, {
     page_path: url,
     ...dataObject
   })
